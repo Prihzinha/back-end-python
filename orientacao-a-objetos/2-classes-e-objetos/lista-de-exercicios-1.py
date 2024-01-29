3c969ff25439e504534b2af0b10fb8fcdbb45dac
Prihzinha <priscilaraguiar@gmail.com>
2024-01-29 18:12:59 -0300

    exercicio classes e ojetos

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -2,12 +2,38 @@
 # Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
 # Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
 
+class Carro: # Convenção para nomes de classes: PascalCasing
+    def __init__(self, cor, modelo):
+        self.ligado = False
+        self.cor = cor
+        self.modelo = modelo
+        self.velocidade = 0
 
+def liga(self):
+    self.ligado = True
+
+def desliga(self):
+        self.ligado = False
+        self.velocidade = 0
+
+def acelera(self):
+    if self.ligado:
+        self.velocidade += 10
+
+def desacelera(self):
+    if self.ligado and self.velocidade > 0:
+        self.velocidade -= 10
 
 # Crie uma instância da classe carro.
-
+        
+meu_carro = Carro('branco','suv')
 
 # Faça o carro "andar" utilizando os métodos da sua classe.
 
+meu_carro.liga()
+meu_carro.acelera()
 
 # Faça o carro "parar" utilizando os métodos da sua classe.
+
+meu_carro.desacelera()
+meu_carro.desliga()
